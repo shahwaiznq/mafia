9230d8e1a916820bb32cebc6f5a41ac6857c709b
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-08-06 00:30:51 +1000

    room joiner using forms

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -10,29 +10,65 @@ require('firebase/database');
 
 export class Room extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             address: '',
-            live: undefined
+            live: undefined,
+            rooms: {},
+            name: '',
+            sendToGame: false
         }
 
+        this.roomCreator = this.roomCreator.bind(this);
+        this.roomJoiner = this.roomJoiner.bind(this);
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({sendToGame: true});
+    }
+
+    handleChange = (event) => {
+        this.setState({name: event.target.value});
     }
 
     roomCreator = () => {
+        return(
+            <div>
+                <h1>Create Room</h1>
+            </div>
+        )
+    }
 
+    roomJoiner = () => {
+        return(
+            <div>
+                <h1>Join Room</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Player Name: <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        )
     }
 
     componentDidMount() {
-        let rooms = {};
+
         this.setState({address: this.props.match.params.name });
-        db.ref('rooms').on("value", function(snapshot) {
-            console.log(snapshot.val());
-        });
-        console.log(rooms);
-        if(rooms[this.state.address]){
-            this.setState = true;
-        }
+
+        db.ref('/rooms').on("value", function(snapshot) {
+            this.setState({rooms: snapshot.val() });
+            if(this.state.rooms[this.state.address]){
+                this.setState({live: true});
+            } else {
+                this.setState({live: false});
+            }
+        }.bind(this));
+
     }
 
     render() {
@@ -41,7 +77,7 @@ export class Room extends Component {
             <div>
                 <h1>Game Room</h1>
                 <h2>Room name: {this.state.address}</h2>
-                {}
+                {this.state.live ? this.roomJoiner() : this.roomCreator() }
             </div>
         )
     }
